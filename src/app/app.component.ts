f016886b3bd1b8142874a3a26a2af57c9b413b51
Teerawat Kuikum <teerawat.k@entronica.co.th>
2017-06-07 10:40:53 +0700

    update leftbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Alenz';
   user = {name: 'non', password: 'non'};
+  result;
+  Password;
+  
+  Yes(Username,Password){
+    // alert('Yes do it')
+    if(Password == '1234'){
+      this.result = 'Log in Success';
+    }
+    else
+      this.result = Username + ' ,Check U Pass.';
+      this.Password ="";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -6,6 +7,7 @@ import { PostComponent } from './post/post.component';
 import { TopicComponent } from './topic/topic.component';
 import { LeftbarComponent } from './leftbar/leftbar.component';
 import { ViewComponent } from './view/view.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -15,11 +17,13 @@ import { ViewComponent } from './view/view.component';
     PostComponent,
     TopicComponent,
     LeftbarComponent,
-    ViewComponent
+    ViewComponent,
+    LoginComponent
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
